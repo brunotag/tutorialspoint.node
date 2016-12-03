17a9829de36a6df6b3ddb6232e4c5f5b1205c616
bruno.tagliapietra <bruno.tagliapietra@gmail.com>
2016-12-04 00:41:15 +1300

    11 Express Framework

---
+++ 11ExpressFramework/07cookies.js
@@ -0,0 +1,28 @@
+var express      = require('express')
+var cookieParser = require('cookie-parser')
+
+var app = express()
+app.use(cookieParser())
+
+// set a cookie
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } 
+  else
+  {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
+
+
+app.listen(8081)
